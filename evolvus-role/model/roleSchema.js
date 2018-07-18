8401117f7bb2f8104f83c38c702f39080c0624e3
Vignesh P <EVOLVUS\vigneshp@EPCBLR-052.evolvus.com>
2018-07-18 18:16:39 +0530

    log removed

--- evolvus-role/model/roleSchema.js
+++ evolvus-role/model/roleSchema.js
@@ -24,7 +24,7 @@ var roleSchema = {
       "type": "array",
       "items": {
         "type": "string"
-      }
+      },
       "filterable": false, //custom attributes
       "sortable": false //custom attribute
     },
