be50b8b35433a317801b37f0cd00362a596c4d16
KmKavya <kavyavshaiva@gmail.com>
2018-07-20 14:15:07 +0530

    role schema changes

--- evolvus-user/model/userSchema.js
+++ evolvus-user/model/userSchema.js
@@ -198,192 +198,206 @@ var userSchema = {
         "tenantId": {
           "type": "string",
           "maxLength": 64,
-          "filterable": true,
-          "sortable": false
+          "filterable": true, //custom attributes
+          "sortable": false //custom attribute
+        },
+        "roleType": {
+          "type": "string",
+          "minLength": 5,
+          "maxLength": 30,
+          "filterable": true, //custom attributes
+          "sortable": false //custom attribute
+        },
+        "txnType": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          },
+          "filterable": false, //custom attributes
+          "sortable": false //custom attribute
+        },
+        "wfInstanceId": {
+          "type": "string",
+          "minLength": 3,
+          "maxLength": 20
+        },
+        "wfInstanceStatus": {
+          "type": "string",
+          "minLength": 3,
+          "maxLength": 20
         },
         "applicationCode": {
           "type": "string",
           "minLength": 3,
           "maxLength": 20,
-          "filterable": true,
-          "sortable": true
+          "filterable": true, //custom attributes
+          "sortable": true //custom attributes
         },
         "roleName": {
           "type": "string",
           "minLength": 1,
           "maxLength": 100,
-          "filterable": true,
-          "sortable": true
+          "pattern": "^[a-zA-Z-0-9-_ ]+$",
+          "message": "RoleName can contain only alphanumeric and two specialcharacters hyphen and underscore",
+          "filterable": true, //custom attributes
+          "sortable": true //custom attributes
         },
         "enableFlag": {
           "type": "string",
-          "enum": [
-            "0",
-            "1"
-          ],
-          "filterable": true,
-          "sortable": true
+          "enum": ["0", "1"],
+          "filterable": true, //custom attributes
+          "sortable": true //custom attributes
         },
         "createdBy": {
           "type": "string",
-          "filterable": false,
-          "sortable": true
+          "filterable": false, //custom attributes
+          "sortable": true //custom attributes
         },
         "updatedBy": {
           "type": "string",
-          "filterable": false,
-          "sortable": true
+          "filterable": false, //custom attributes
+          "sortable": true //custom attributes
         },
         "createdDate": {
           "type": "string",
           "format": "date-time",
-          "filterable": true,
-          "sortable": true
+          "filterable": true, //custom attributes
+          "sortable": true //custom attributes
         },
         "lastUpdatedDate": {
-          "type": [
-            "string",
-            "null"
-          ],
+          "type": ["string", "null"],
           "format": "date-time",
-          "filterable": false,
-          "sortable": true
+          "filterable": false, //custom attributes
+          "sortable": true //custom attributes
         },
         "selectedFlag": {
           "type": "string",
-          "enum": [
-            "0",
-            "1"
-          ],
-          "filterable": false,
-          "sortable": false
+          "enum": ["0", "1"],
+          "filterable": false, //custom attributes
+          "sortable": false //custom attributes
         },
         "activationStatus": {
           "type": "string",
-          "enum": [
-            "ACTIVE",
-            "INACTIVE"
-          ],
+          "enum": ["ACTIVE", "INACTIVE"],
           "displayable": true,
-          "filterable": true,
-          "sortable": false
+          "filterable": true, //custom attributes
+          "sortable": false //custom attributes
         },
         "processingStatus": {
           "type": "string",
-          "enum": [
-            "PENDING_AUTHORIZATION",
-            "AUTHORIZED",
-            "REJECTED"
-          ],
-          "default": "PENDING_AUTHORIZATION",
+          "enum": ['PENDING_AUTHORIZATION', 'AUTHORIZED', 'REJECTED'],
+          "default": 'PENDING_AUTHORIZATION',
           "displayable": true,
-          "filterable": true,
-          "sortable": true
+          "filterable": true, //custom attributes
+          "sortable": true //custom attributes
         },
         "associatedUsers": {
           "type": "number",
-          "filterable": false,
-          "sortable": false,
-          "displayable": true
+          "filterable": false, //custom attributes
+          "sortable": false //custom attributes
         },
         "accessLevel": {
           "type": "string",
-          "filterable": true,
-          "sortable": false,
-          "displayable": false
+          "filterable": true, //custom attributes
+          "sortable": false //custom attributes
         },
         "entityId": {
           "type": "string",
-          "filterable": true,
-          "sortable": false
+          "filterable": true, //custom attributes
+          "sortable": false //custom attributes
         },
         "menuGroup": {
           "type": "array",
+          "minItems": 1,
+          "message": "Menus are required",
           "items": {
-            "type": "object",
             "properties": {
               "tenantId": {
                 "type": "string",
                 "minLength": 1,
                 "maxLength": 64,
-                "filterable": false,
-                "sortable": false
+                "filterable": false, //custom attributes
+                "sortable": false //custom attributes
               },
               "applicationCode": {
                 "type": "string",
                 "minLength": 3,
                 "maxLength": 20,
-                "filterable": true,
-                "sortable": false
+                "filterable": true, //custom attributes
+                "sortable": false //custom attributes
               },
               "menuGroupCode": {
                 "type": "string",
                 "minLength": 1,
                 "maxLength": 20,
-                "filterable": false,
-                "sortable": false
+                "filterable": false, //custom attributes
+                "sortable": false //custom attributes
               },
               "title": {
                 "type": "string",
                 "minLength": 1,
                 "maxLength": 20,
-                "filterable": false,
-                "sortable": false
+                "filterable": false, //custom attributes
+                "sortable": false //custom attributes
               },
               "createdBy": {
                 "type": "string",
-                "filterable": false,
-                "sortable": false
+                "filterable": false, //custom attributes
+                "sortable": false //custom attributes
               },
               "updatedBy": {
                 "type": "string",
-                "filterable": false,
-                "sortable": false
+                "filterable": false, //custom attributes
+                "sortable": false //custom attributes
               },
               "createdDate": {
                 "type": "string",
                 "format": "date-time",
-                "filterable": false,
-                "sortable": false
+                "filterable": false, //custom attributes
+                "sortable": false //custom attributes
               },
               "lastUpdatedDate": {
                 "type": "string",
                 "format": "date-time",
-                "filterable": false,
-                "sortable": false
+                "filterable": false, //custom attributes
+                "sortable": false //custom attributes
               },
               "enableFlag": {
                 "type": "string",
-                "enum": [
-                  "0",
-                  "1"
-                ],
-                "filterable": false,
-                "sortable": false
+                "enum": ["0", "1"],
+                "filterable": false, //custom attributes
+                "sortable": false //custom attributes
               },
               "menuGroupOrder": {
                 "type": "number",
-                "filterable": false,
-                "sortable": false
+                "filterable": false, //custom attributes
+                "sortable": false //custom attributes
               },
               "menuItems": {
                 "type": "array",
+                "minItems": 1,
                 "items": {
-                  "type": "object",
                   "properties": {
                     "menuItemType": {
                       "type": "string",
                       "minLength": 1,
                       "maxLength": 20,
-                      "filterable": false,
-                      "sortable": false
+                      "filterable": false, //custom attributes
+                      "sortable": false //custom attributes
                     },
                     "applicationCode": {
                       "type": "string",
                       "minLength": 3,
                       "maxLength": 20,
-                      "filterable": true,
-                      "sortable": false
+                      "filterable": true, //custom attributes
+                      "sortable": false //custom attributes
+                    },
+                    "menuItemCode": {
+                      "type": "string",
+                      "minLength": 1,
+                      "maxLength": 20,
+                      "filterable": false, //custom attributes
+                      "sortable": false //custom attributes
                     },
                     "icon": {
                       "type": "string",
@@ -395,55 +409,34 @@ var userSchema = {
                       "minLength": 0,
                       "maxLength": 30
                     },
-                    "menuItemCode": {
-                      "type": "string",
-                      "minLength": 1,
-                      "maxLength": 20,
-                      "filterable": false,
-                      "sortable": false
-                    },
                     "title": {
                       "type": "string",
                       "minLength": 1,
                       "maxLength": 20,
-                      "filterable": false,
-                      "sortable": false
+                      "filterable": false, //custom attributes
+                      "sortable": false //custom attributes
                     },
                     "menuItemOrder": {
                       "type": "number",
-                      "filterable": false,
-                      "sortable": false
+                      "required": "true",
+                      "filterable": false, //custom attributes
+                      "sortable": false //custom attributes
                     }
-                  }
-                },
-                "required": [
-                  "menuItemType",
-                  "applicationCode",
-                  "menuItemCode",
-                  "title",
-                  "menuItemOrder"
-                ]
+                  },
+                  "required": ["menuItemType", "applicationCode", "menuItemCode", "title", "menuItemOrder"]
+                }
               }
-            }
-          },
-          "required": [
-            "tenantId",
-            "applicationCode",
-            "menuGroupCode",
-            "menuGroupOrder",
-            "title",
-            "createdDate",
-            "createdBy",
-            "menuItems"
-          ]
+            },
+            "required": ["tenantId", "applicationCode", "menuGroupCode", "menuGroupOrder", "title", "createdDate", "createdBy", "menuItems"]
+          }
         },
         "description": {
           "type": "string",
           "minLength": 0,
           "maxLength": 255,
-          "pattern": "^[ A-Za-z0-9_@./#&+-]*",
-          "filterable": false,
-          "sortable": false,
+          "pattern": "^[ A-Za-z0-9_@.,;:/&!^*(){}[\]?$%#&=+-]*$",
+          "filterable": false, //custom attributes
+          "sortable": false, //custom attributes
           "displayable": true
         }
       },
@@ -489,12 +482,10 @@ var userSchema = {
         },
         "phoneNumber": {
           "type": "string",
-          "minLength": 10,
           "maxLength": 10
         },
         "mobileNumber": {
           "type": "string",
-          "minLength": 10,
           "maxLength": 10
         },
         "mobileVerified": {
