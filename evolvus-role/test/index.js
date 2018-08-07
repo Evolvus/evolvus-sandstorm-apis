ebdf1bcdb20dfb8945e37521824461b8cc0c4057
pavithra30 <EVOLVUS\kamalaranip@EPCBLR-044.evolvus.com>
2018-08-07 19:12:56 +0530

    installed latest version of docket-client

--- evolvus-role/test/index.js
+++ evolvus-role/test/index.js
@@ -2,6 +2,7 @@ const debug = require("debug")("evolvus-role.test.index");
 const chai = require("chai");
 const mongoose = require("mongoose");
 const dbSchema = require("../db/roleSchema");
+const _ = require("lodash");
 
 var MONGO_DB_URL = process.env.MONGO_DB_URL || "mongodb://10.10.69.204:27017/TestPlatform_Dev";
 /*
@@ -23,8 +24,11 @@ const collection = new Dao("role", dbSchema);
 const roleTestData = require("./roleTestData");
 const tenantOne = "IVL";
 const tenantTwo = "KOT";
+const entityId = "H001B001";
+const accessLevel = "1";
+
 describe('role model validation', () => {
-  
+
   let invalidObject = {
     //add invalid role Object here
     "tenantId": "IVL",
@@ -71,6 +75,7 @@ describe('role model validation', () => {
   });
 
   describe("validation against jsonschema", () => {
+
     it("valid role should validate successfully", (done) => {
       try {
         const validObject1 = roleTestData.validObject1;
@@ -80,12 +85,11 @@ describe('role model validation', () => {
           .notify(done);
         // if notify is not done the test will fail
         // with timeout
-  
       } catch (e) {
         expect.fail(e, null, `valid role object should not throw exception: ${e}`);
       }
     });
-  
+
     it("invalid role should return errors", (done) => {
       try {
         var res = role.validate(invalidObject);
@@ -96,38 +100,54 @@ describe('role model validation', () => {
         expect.fail(e, null, `exception: ${e}`);
       }
     });
-  
-    if ("should error out for undefined objects", (done) => {
-        try {
-          var res = role.validate(undefinedObject);
-          expect(res)
-            .to.be.rejected
-            .notify(done);
-        } catch (e) {
-          expect.fail(e, null, `exception: ${e}`);
-        }
-      });
-  
-    if ("should error out for null objects", (done) => {
-        try {
-          var res = role.validate(nullObject);
-          expect(res)
-            .to.be.rejected
-            .notify(done);
-        } catch (e) {
-          expect.fail(e, null, `exception: ${e}`);
-        }
-      });
+
+    it("should error out for undefined objects", (done) => {
+      try {
+        var res = role.validate(undefinedObject);
+        expect(res)
+          .to.be.rejected
+          .notify(done);
+      } catch (e) {
+        expect.fail(e, null, `exception: ${e}`);
+      }
+    });
+
+    it("should error out for null objects", (done) => {
+      try {
+        var res = role.validate(nullObject);
+        expect(res)
+          .to.be.rejected
+          .notify(done);
+      } catch (e) {
+        expect.fail(e, null, `exception: ${e}`);
+      }
+    });
+
   });
 
   describe("testing role.save method", () => {
 
+    beforeEach(function(done) {
+      this.timeout(10000);
+      collection.deleteAll({
+          tenantId: tenantOne
+        })
+        .then((data) => {
+          return collection.deleteAll({
+            tenantId: tenantTwo
+          });
+        })
+        .then((data) => {
+          done();
+        });
+    });
+
     it('should save a valid role object to database', (done) => {
       try {
-        var result = role.save("T001", "user", "192.168.1.122", "1", "H001B001", roleTestData.validObject1);
-        //replace anyAttribute with one of the valid attribute of a entity Object
+        var result = role.save(tenantOne, "user", "192.168.1.122", "1", "H001B001", roleTestData.validObject1);
         expect(result)
-          .to.eventually.have.property("_id")
+          .to.eventually.have.property("nModified")
+          .to.equal(1)
           .notify(done);
       } catch (e) {
         expect.fail(e, null, `saving role object should not throw exception: ${e}`);
@@ -144,35 +164,48 @@ describe('role model validation', () => {
         expect.fail(e, null, `exception: ${e}`);
       }
     });
-  });
 
+  });
 
   describe("testing update Role", () => {
     beforeEach((done) => {
-      collection.deleteAll({"tenantId":tenantOne})
-        .then((value) => {
-          return collection.deleteAll({"tenantId":tenantTwo});
+      collection.deleteAll({
+          tenantId: tenantOne
         })
         .then((value) => {
-          return collection.save(roleTestData.validObject1);
+          return collection.deleteAll({
+            tenantId: tenantTwo
+          });
         })
         .then((value) => {
-          return collection.save(roleTestData.validObject2);
+          return collection.save(_.merge(roleTestData.validObject1, {
+            "tenantId": tenantOne
+          }));
         })
         .then((value) => {
-          return collection.save(roleTestData.validObject3);
+          return collection.save(_.merge(roleTestData.validObject2, {
+            "tenantId": tenantOne
+          }));
         })
         .then((value) => {
-          return collection.save( roleTestData.validObject4);
+          return collection.save(_.merge(roleTestData.validObject3, {
+            "tenantId": tenantOne
+          }));
+        })
+        .then((value) => {
+          return collection.save(_.merge(roleTestData.validObject4, {
+            "tenantId": tenantOne
+          }));
         })
         .then((value) => {
           done();
         });
     });
+
     it('should update a role with new values', (done) => {
-      var res = role.update("T001", "Admin_One11",{
+      var res = role.update(tenantOne, "user", "192.168.1.122", "ADMIN_ONE", {
         "enableFlag": true,
-        "roleName": "Admin_One11",
+        "roleName": "ADMIN_ONE",
         "description": "test update"
       });
       expect(res)
@@ -184,59 +217,383 @@ describe('role model validation', () => {
           done();
         });
     });
+
     it("should throw IllegalArgumentException for undefined tenantId parameter ", (done) => {
       let undefinedId;
-      let res = role.update(undefinedId, "admin_One", {
+      let res = role.update(undefinedId, "user", "192.168.1.122", "ADMIN_ONE", {
         roleName: "Admin"
       });
       expect(res)
         .to.eventually.to.be.rejectedWith("IllegalArgumentException")
         .notify(done);
     });
-  
+
     it("should throw IllegalArgumentException for undefined code parameter ", (done) => {
       // an id is a 12 byte string, -1 is an invalid id value+
       let undefinedCode;
-      let res = role.update(tenantOne, undefinedCode, null);
+      let res = role.update("T001", "user", "192.168.1.122", undefinedCode, null);
       expect(res)
         .to.eventually.to.be.rejectedWith("IllegalArgumentException")
         .notify(done);
     });
-  
+
     it("should throw IllegalArgumentException for undefined update parameter ", (done) => {
       let undefinedUpdate;
-      let res = role.update(tenantOne, "admin_One", undefinedUpdate);
+      let res = role.update(tenantOne, "user", "192.168.1.122", "admin_One", undefinedUpdate);
       expect(res)
         .to.eventually.to.be.rejectedWith("IllegalArgumentException")
         .notify(done);
     });
-  
+
     it("should throw IllegalArgumentException for null tenantId parameter ", (done) => {
       // an id is a 12 byte string, -1 is an invalid id value+
-      let res = role.update(null, "admin_One", {
-        roleName: "Admin"
+      let res = role.update(null, "user", "192.168.1.122", "ADMINN", {
+        "enableFlag": true,
+        "roleName": "ADMINN",
+        "description": "test update"
       });
       expect(res)
         .to.eventually.to.be.rejectedWith("IllegalArgumentException")
         .notify(done);
     });
-  
+
     it("should throw IllegalArgumentException for null code parameter ", (done) => {
       // an id is a 12 byte string, -1 is an invalid id value+
-      let res = role.update(tenantOne, null, {
-        roleName: "Admin"
+      let res = role.update("T001", "user", "192.168.1.122", null, {
+        "enableFlag": true,
+        "roleName": "ADMINN",
+        "description": "test update"
       });
       expect(res)
         .to.eventually.to.be.rejectedWith("IllegalArgumentException")
         .notify(done);
     });
-  
+
     it("should throw IllegalArgumentException for null update parameter ", (done) => {
       // an id is a 12 byte string, -1 is an invalid id value+
-      let res = role.update(tenantOne, "admin_One", null);
+      let res = role.update("T001", "user", "192.168.1.122", "ADMINN", null);
       expect(res)
         .to.eventually.to.be.rejectedWith("IllegalArgumentException")
         .notify(done);
     });
+
+    it("should disable role ADMIN_ONE", (done) => {
+      let res = role.update(tenantOne, "user", "192.168.1.122", "ADMIN_ONE", {
+        "enableFlag": "false"
+      });
+      expect(res)
+        .to.have.be.fulfilled.then((role) => {
+          debug("result: " + JSON.stringify(role));
+          expect(role)
+            .to.have.property("nModified")
+            .to.equal(1);
+          done();
+        });
+    });
+
+    it("should not update the already exists roleName", (done) => {
+      let res = role.update(tenantOne, "user", "192.168.1.122", "ADMIN_ONE", {
+        "roleName": "ADMIN_ONE"
+      });
+      expect(res)
+        .to.have.be.fulfilled.then((app) => {
+          debug("result: " + JSON.stringify(app));
+          expect(app)
+            .to.have.property("nModified")
+            .to.equal(1);
+          done();
+        });
+    });
+
+    it("should be rejected if no role found", (done) => {
+      let res = role.update(tenantOne, "user", "192.168.1.122", "sff654dgh", {
+        "description": "jkhdfugf"
+      });
+      expect(res)
+        .to.be.rejectedWith(`Role SFF654DGH, not found `)
+        .notify(done);
+    });
+
   });
+
+  describe('testing role.find', () => {
+
+    let object1 = roleTestData.validObject1,
+      object2 = roleTestData.validObject2,
+      object3 = roleTestData.validObject3,
+      object4 = roleTestData.validObject4,
+      object5 = roleTestData.validObject5;
+
+    beforeEach(function(done) {
+      this.timeout(10000);
+      collection.deleteAll({
+          tenantId: tenantOne
+        })
+        .then((value) => {
+          return collection.deleteAll({
+            tenantId: tenantTwo
+          });
+        })
+        .then((value) => {
+          return collection.save(_.merge(object1, {
+            "tenantId": tenantOne
+          }));
+        })
+        .then((value) => {
+          return collection.save(_.merge(object2, {
+            "tenantId": tenantOne
+          }));
+        })
+        .then((value) => {
+          return collection.save(_.merge(object3, {
+            "tenantId": tenantOne
+          }));
+        })
+        .then((value) => {
+          return collection.save(_.merge(object4, {
+            "tenantId": tenantOne
+          }));
+        })
+        .then((value) => {
+          return collection.save(_.merge(object5, {
+            "tenantId": tenantOne
+          }));
+        })
+        .then((value) => {
+          return collection.save(_.merge(object1, {
+            "tenantId": tenantTwo
+          }));
+        })
+        .then((value) => {
+          return collection.save(_.merge(object2, {
+            "tenantId": tenantTwo
+          }));
+        })
+        .then((value) => {
+          return collection.save(_.merge(object3, {
+            "tenantId": tenantTwo
+          }));
+        })
+        .then((value) => {
+          return collection.save(_.merge(object4, {
+            "tenantId": tenantTwo
+          }));
+        })
+        .then((value) => {
+          return collection.save(_.merge(object5, {
+            "tenantId": tenantTwo
+          }));
+        })
+        .then((value) => {
+          done();
+        });
+    });
+
+    it("should return all the values of tenant One only", (done) => {
+      let res = role.find(tenantOne, "user", "198.162.1.122", {}, {}, 0, 0);
+      expect(res)
+        .to.have.be.fulfilled.then((roles) => {
+          expect(roles)
+            .to.have.lengthOf(5);
+          expect(roles[0])
+            .to.have.property("tenantId")
+            .to.equal(tenantOne);
+          expect(roles[1])
+            .to.have.property("tenantId")
+            .to.equal(tenantOne);
+          expect(roles[2])
+            .to.have.property("tenantId")
+            .to.equal(tenantOne);
+          expect(roles[3])
+            .to.have.property("tenantId")
+            .to.equal(tenantOne);
+          expect(roles[4])
+            .to.have.property("tenantId")
+            .to.equal(tenantOne);
+          done();
+        });
+    });
+
+    it("should return all the values of tenant Two only", (done) => {
+      let res = role.find(tenantTwo, "user", "198.162.1.122", {}, {}, 0, 0);
+      expect(res)
+        .to.have.be.fulfilled.then((roles) => {
+          expect(roles)
+            .to.have.lengthOf(5);
+          expect(roles[0])
+            .to.have.property("tenantId")
+            .to.equal(tenantTwo);
+          expect(roles[1])
+            .to.have.property("tenantId")
+            .to.equal(tenantTwo);
+          expect(roles[2])
+            .to.have.property("tenantId")
+            .to.equal(tenantTwo);
+          expect(roles[3])
+            .to.have.property("tenantId")
+            .to.equal(tenantTwo);
+          expect(roles[4])
+            .to.have.property("tenantId")
+            .to.equal(tenantTwo);
+          done();
+        });
+    });
+
+    //There are two roles with same roleName as admin_One and different tenantId
+    //It should return only one role from one tenant
+    it("should return a role Object from tenantTwo", (done) => {
+      var filter = {
+        roleName: "ADMIN_ONE"
+      };
+      let res = role.find(tenantTwo, "user", "192.168.1.122", filter, {}, 0, 0);
+      expect(res)
+        .to.have.be.fulfilled.then((roles) => {
+          expect(roles[0])
+            .to.have.property("tenantId")
+            .to.equal(tenantTwo);
+          expect(roles[0])
+            .to.have.property("roleName")
+            .to.equal("ADMIN_ONE");
+          done();
+        });
+    });
+
+    it("should return a single value of a tenantOne", (done) => {
+      let res = role.find(tenantOne, "user", "192.168.1.122", {
+        "roleName": "ADMIN_ONE"
+      }, {}, 0, 1);
+      expect(res)
+        .to.have.be.fulfilled.then((role) => {
+          debug("result:" + JSON.stringify(role));
+          expect(role[0])
+            .to.have.property("tenantId")
+            .to.equal(tenantOne);
+          expect(role[0])
+            .to.have.property("roleName")
+            .to.equal("ADMIN_ONE");
+          done();
+        });
+    });
+
+    it("should return ADMIN_ONE, the first role when sorted by roleName", (done) => {
+      let res = role.find(tenantOne, "user", "192.168.1.122", {}, {
+        "roleName": 1
+      }, 0, 1);
+      expect(res)
+        .to.have.be.fulfilled.then((role) => {
+          debug("result: " + JSON.stringify(role));
+          expect(role[0])
+            .to.have.property("tenantId")
+            .to.equal(tenantOne);
+          expect(role[0])
+            .to.have.property("roleName")
+            .to.equal("ADMIN_ONE");
+          done();
+        });
+    });
+
+    it("should return admin_Two, the last role when sorted by roleName", (done) => {
+      let res = role.find(tenantOne, "user", "192.168.1.122", {}, {
+        "roleName": -1
+      }, 0, 1);
+      expect(res)
+        .to.have.be.fulfilled.then((role) => {
+          debug("result: " + JSON.stringify(role));
+          expect(role[0])
+            .to.have.property("tenantId")
+            .to.equal(tenantOne);
+          expect(role[0])
+            .to.have.property("roleName")
+            .to.equal("admin_Two");
+          done();
+        });
+    });
+
+    it("should return 3 enabled roles", (done) => {
+      let res = role.find(tenantOne, "user", "192.168.1.122", {
+        "enableFlag": "true"
+      }, {
+        "roleName": -1
+      }, 0, 10);
+      expect(res)
+        .to.have.be.fulfilled.then((role) => {
+          debug("result: " + JSON.stringify(role));
+          expect(role)
+            .to.have.lengthOf(3);
+          expect(role[0])
+            .to.have.property("tenantId")
+            .to.equal(tenantOne);
+          expect(role[0])
+            .to.have.property("roleName")
+            .to.equal("admin_Two");
+          expect(role[2])
+            .to.have.property("roleName")
+            .to.equal("ADMIN_ONE");
+          done();
+        });
+    });
+
+    it("should return all roles from tenantOne if accessLevel is 1", (done) => {
+      let res = role.find(tenantOne, "user", "192.168.1.122", {}, {}, 0, 10);
+      expect(res)
+        .to.have.be.fulfilled.then((roles) => {
+          debug("result: " + JSON.stringify(roles));
+          expect(roles)
+            .to.have.lengthOf(5);
+          expect(roles[0])
+            .to.have.property("tenantId")
+            .to.equal(tenantOne);
+          done();
+        });
+    });
+
+    it("should return only 2 roles from tenantOne if accessLevel is 2", (done) => {
+      let res = role.find(tenantOne, "user", "192.168.1.122", {
+        accessLevel: "2"
+      }, {
+        roleName: -1
+      }, 0, 10);
+      expect(res)
+        .to.have.be.fulfilled.then((roles) => {
+          debug("result: " + JSON.stringify(roles));
+          expect(roles)
+            .to.have.lengthOf(2);
+          expect(roles[0])
+            .to.have.property("tenantId")
+            .to.equal(tenantOne);
+          expect(roles[0])
+            .to.have.property("roleName")
+            .to.equal(object2.roleName);
+          expect(roles[1])
+            .to.have.property("roleName")
+            .to.equal(object3.roleName);
+          done();
+        });
+    });
+
+    it('should throw IllegalArgumentException for null value of tenantId', (done) => {
+      try {
+        let res = role.find(null, "user", "192.168.1.122", {}, {}, 0, 1);
+        expect(res)
+          .to.be.rejectedWith("IllegalArgumentException")
+          .notify(done);
+      } catch (e) {
+        expect.fail(e, null, `exception: ${e}`);
+      }
+    });
+
+    it('should throw IllegalArgumentException for undefined value of tenantId', (done) => {
+      var undefinedId;
+      try {
+        let res = role.find(undefinedId, "user", "192.168.1.122", {}, {}, 0, 1);
+        expect(res)
+          .to.be.rejectedWith("IllegalArgumentException")
+          .notify(done);
+      } catch (e) {
+        expect.fail(e, null, `exception: ${e}`);
+      }
+    });
+
+  });
+
 });
