7a634fc8c61883bc0ca76ff5b576d0861581ad84
Vignesh P <EVOLVUS\vigneshp@EPCBLR-052.evolvus.com>
2018-08-09 10:21:03 +0530

    backupfile added

---
+++ evolvus-mongo-seed/backupfile.js
@@ -0,0 +1,27 @@
+module.exports={
+    "menuItems": [
+      {
+        "menuItemType": "menu",
+        "applicationCode": "SANDSTORM",
+        "menuItemCode": "bulkUpload",
+        "title": "Bulk Upload",
+        "menuItemOrder": 3,
+        "selectedFlag": false,
+        "icon": "evo-bulkupload",
+        "link": "bulkUpload",
+        "subMenuItems": []
+      }],
+      "deletedFlag": 0,
+      "menuGroupCode": "maintenance",
+      "title": "MAINTENANCE",
+      "applicationCode": "SANDSTORM",
+      "tenantId": "$tenantId",
+      "createdBy": "SYSTEM",
+      "menuGroupOrder": 2,
+      "createdDate": new Date(),
+      "lastUpdatedDate": new Date(),
+      "wfInstanceId": "",
+      "wfInstanceStatus": "",
+      "__v": 0
+    
+  }
