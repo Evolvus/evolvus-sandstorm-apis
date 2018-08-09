9bee8e3935254a63394f8fabf3e64f3b2d679f9b
Vignesh P <EVOLVUS\vigneshp@EPCBLR-052.evolvus.com>
2018-08-09 12:44:27 +0530

    mongo-seed Added

---
+++ evolvus-mongo-seed/routes/api/mongoSeed.js
@@ -0,0 +1,55 @@
+var fs = require('fs');
+const fse = require('fs-extra');
+const mongoSeed = require('./../../index.js');
+var Engine = require('velocity').Engine
+
+const {
+  seedDatabase
+} = require('mongo-seeding');
+
+var configuration = mongoSeed.dbconfigure;
+var fileConfig = mongoSeed.fileconfigure;
+
+module.exports = (router) => {
+  router.route('/mongoSeed')
+    .post((req, res, next) => {
+      const ctx = {
+        tenantId: req.query.tenantId,
+        entityId: req.query.entityId || 'H001B001'
+      };
+
+      create(fileConfig, ctx, () => {
+
+        seedDatabase(configuration).then(() => {
+          res.send("Your SeedData Added SuccessFully");
+        }).catch(err => {
+          res.send("Unable to Add SeedData");
+        });
+
+      });
+    });
+};
+
+function create(files, ctx, callback) {
+
+
+  files.forEach(file => {
+
+    const outputdir = file.outputdir;
+
+    const finalDir = `${outputdir}/${file.mkpath}`;
+    if (!fs.existsSync(finalDir)) {
+
+      fs.mkdirSync(finalDir);
+
+    }
+
+    var engine = new Engine({
+      "template": file.template,
+      "output": `${finalDir}/${file.fileName}`
+    });
+    var result = engine.render(ctx);
+
+  });
+  callback();
+}
